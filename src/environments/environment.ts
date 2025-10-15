98f444d517178960c70483e32e43b851944aa891
mskazi <mskazikis@gmail.com>
2025-10-15 20:58:29 +0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment: {
+  appwriteEndpoint: string;
+  appwriteProjectId: string;
+  appwriteProjectName: string;
+} = {
+  appwriteEndpoint: 'https://fra.cloud.appwrite.io/v1',
+  appwriteProjectId: '68efddd6001f961fa2a9',
+  appwriteProjectName: 'Lottery'
+};
